dbe75219c115b25c12d16cca7ecfdb071d9de76f
BensonGitau <bensonkamaugitau@gmail.com>
2024-06-29 10:28:01 +0300

    complete

--- server/models.py
+++ server/models.py
@@ -20,15 +20,33 @@ db = SQLAlchemy(metadata=metadata)
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
+    serialize_rules = ('-animals.zookeeper',)
+    #this can be used when you want to specify specific fields to serialize
+    #serialize_only = ('id', 'name', 'animals.name', 'animals.species',)
+
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     birthday = db.Column(db.Date)
 
     animals = db.relationship('Animal', back_populates='zookeeper')
 
+    def __repr__(self):
+        return f'<Zookeeper {self.name} {self.birthday}>'
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'birthday': self.birthday,
+            'animals': [animal.to_dict() for animal in self.animals]
+        }
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
+    
+    serialize_rules = ('-animals.enclosure',)
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
@@ -36,9 +54,19 @@ class Enclosure(db.Model):
 
     animals = db.relationship('Animal', back_populates='enclosure')
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'environment': self.environment,
+            'open_to_visitors': self.open_to_visitors,
+            'animlas': [animal.to_dict() for animal in self.animals]
+        }
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
+
+    serilaize_rules= ('-zookeeper.animals', '-enclosure.animals',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -49,6 +77,13 @@ class Animal(db.Model):
 
     enclosure = db.relationship('Enclosure', back_populates='animals')
     zookeeper = db.relationship('Zookeeper', back_populates='animals')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'species': self.species
+        }
 
     def __repr__(self):
         return f'<Animal {self.name}, a {self.species}>'
